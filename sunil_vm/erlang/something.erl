f1886699495af757f602bd6dddda212ca0e20ab5
Sunil <sunil.gandhi007@gmail.com>
2012-11-12 18:51:59 -0500

    Trial file: Simple timer

--- sunil_vm/erlang/something.erl
+++ sunil_vm/erlang/something.erl
@@ -2,9 +2,15 @@
 -compile(export_all).
 -import(matrix).
 
-something(Function,Myvalue, Value)->
-    case Function of 
-        max -> erlang:max(Myvalue,Value);
-        min -> erlang:min(Myvalue,Value);
-        mean -> (Myvalue + Value)/2
-    end.
+something() ->
+	spawn_link(fun() -> myprocess() end),
+
+myprocess()->
+    receive
+    	tick ->
+    		% write code for doing things periodically
+    		io:format("Got one"),
+			timer:send_after(1000, tick)
+		end,
+	myprocess().
+
