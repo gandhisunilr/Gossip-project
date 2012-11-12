510ce8b3a09b16fdf90a713b41a6ff40bb70dd9b
Sunil <sunil.gandhi007@gmail.com>
2012-11-12 15:26:14 -0500

    Recieving of gossip message completed

--- sunil_vm/erlang/gossip.erl
+++ sunil_vm/erlang/gossip.erl
@@ -10,8 +10,13 @@ start()->
 	gossip(max,P).
 
 gossip(Function,TransitionMatrix) ->
-	Pids = create(5,[]),
+	Pids = create(5,[],TransitionMatrix),
 	sendpids(length(Pids),Pids).
+	% hd(Pids) ! {max, self(), 100},
+	% receive
+	% 	{returnmsg, Function, Pid, Value } ->
+	% 		Value
+	% end.
 
 sendpids(0,Pids) -> 1;
 sendpids(I,Pids) ->
@@ -19,10 +24,10 @@ sendpids(I,Pids) ->
 	sendpids(I-1,Pids).
 
 
-create(0,Pids) -> Pids;
-create(I,Pids)->
-	Pid = spawn_link(fun() -> threadnodes(I,[],I) end),
-	create(I-1,  (Pids ++ [Pid]) ).
+create(0,Pids,TransitionMatrix) -> Pids;
+create(I,Pids,TransitionMatrix)->
+	Pid = spawn_link(fun() -> threadnodes(TransitionMatrix,[],I) end),
+	create(I-1,  (Pids ++ [Pid]), TransitionMatrix ).
 
 calculate(Function,Myvalue,Value) ->
 case Function of 
@@ -31,19 +36,27 @@ case Function of
         mean -> (Myvalue + Value)/2
     end.
 
+selectneighbours(I) ->
+	I-1.
+
 threadnodes(TransitionMatrix,Pids,Myvalue) ->
-	%getneighbours
-	%getfragments
+	%getneighbours()
+	%getfragments()
 	receive
-		%get the fucking pids
+		%get the fucking pids of all processes
         {pid, Pids } ->
-        	threadnodes(TransitionMatrix,Pids,Myvalue)
+        	threadnodes(TransitionMatrix,Pids,Myvalue);
+        %Recieve Function from Process Pid with his value
         {Function, Pid, Value } ->
-        	Pid ! { returnmsg, Function, self(), Myvalue }
-        	io:format("Yay I just received ~p and my Value is ~p and I am computing ~p ~n", [Value,Myvalue, Function]).
-        	threadnodes(TransitionMatrix,Pids, erlang:calculate( Function, Myvalue,Value) )
-        {Function, Pid, Value } ->
-        	Pid ! { returnmsg, Function, self(), Myvalue }
-        	io:format("Yay I just received ~p and my Value is ~p and I am computing ~p ~n", [Value,Myvalue, Function]).
-        	threadnodes(TransitionMatrix,Pids, erlang:calculate( Function, Myvalue,Value) )	
+        	Pid ! { returnmsg, Function, self(), Myvalue },
+        	io:format("Yay I just received ~p and my Value is ~p and I am computing ~p ~n", [Value,Myvalue, Function]),
+        	threadnodes(TransitionMatrix,Pids, calculate( Function, Myvalue,Value) );
+        %Reply Recieve Function from Process Pid with his value	
+        {returnmsg, Function, Pid, Value } ->
+        	io:format("Reply ~p and my Value is ~p and I am computing ~p ~n", [Value,Myvalue, Function]),
+        	threadnodes(TransitionMatrix,Pids, erlang:calculate( Function, Myvalue,Value) );
+        %Send Function
+        {start} ->
+        	I=selectneighbours(I),
+        	
     end.
