9ceb7e368d35e293c93247f66ce2aa3b39c46625
Sunil <sunil.gandhi007@gmail.com>
2012-11-21 21:58:28 -0500

    Generating tuples

--- sunil_vm/erlang/something.erl
+++ sunil_vm/erlang/something.erl
@@ -4,11 +4,11 @@
 
 something(FileName) ->
     {ok, Device} = file:open(FileName, [read]),
-    get_all_lines(Device, []).
+    get_all_lines(1,Device, []).
 
-get_all_lines(Device, Accum) ->
+get_all_lines(I,Device, Accum) ->
     case io:fread(Device, "","~f") of
     	eof -> file:close(Device), Accum;
     	{ok, [N]} -> 
-            get_all_lines(Device, Accum ++ [N])
+            get_all_lines(I+1,Device, Accum ++ [{I,N}])
     end.
