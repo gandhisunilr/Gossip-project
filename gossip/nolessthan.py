2020d03348e6296d26bb4e2e3196e9c4591a6b10
Sunil <sunil.gandhi007@gmail.com>
2012-11-27 22:26:29 -0500

    Function for calculating no of elements less than value

--- gossip/nolessthan.py
+++ gossip/nolessthan.py
@@ -2,4 +2,4 @@ def nolessthan(value):
 	with open('randnofile') as f:
 		lines = f.read().splitlines()
 	numbers = list(map(float, lines))
-	len([x for x in numbers if x >= 53088.213058])
+	return len([x for x in numbers if x <= value])
