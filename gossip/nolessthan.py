def7bcb35ccaea9894349d88c8de83f7aee17ddc
Sunil <sunil.gandhi007@gmail.com>
2012-11-27 22:37:21 -0500

    < not <=

--- gossip/nolessthan.py
+++ gossip/nolessthan.py
@@ -2,4 +2,4 @@ def nolessthan(value):
 	with open('randnofile') as f:
 		lines = f.read().splitlines()
 	numbers = list(map(float, lines))
-	return len([x for x in numbers if x <= value])
+	return len([x for x in numbers if x < value])
