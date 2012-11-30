fed36944ef44254a305591c52253fa810ef6797f
Primal Pappachan <primal007@gmail.com>
2012-11-29 23:31:56 -0500

    Replication Factor added

--- gossip/fragreader.erl
+++ gossip/fragreader.erl
@@ -1,10 +1,10 @@
 -module(fragreader).
 -compile(export_all).
 
-genfrags(Nodes) ->
+genfrags(Nodes, Factor) ->
     {ok, Device} = file:open(randnofile, [read]),
         Parts = get_all_lines(Device, [], 1),
-            fragmentize(Parts, length(Parts),[], Nodes).
+            fragmentize(Parts, length(Parts),[], Nodes, Factor).
 
 get_all_lines(Device, Accum, Index) ->
 	case io:fread(Device, "","~f") of
@@ -13,29 +13,30 @@ get_all_lines(Device, Accum, Index) ->
              	get_all_lines(Device, Accum ++ [{Index, N}], Index+1)
 	end.
 
-fragmentize(Parts, Len, Fragments, Nodes) when length(Fragments) < Nodes ->
+fragmentize(Parts, Len, Fragments, Nodes, Factor) when length(Fragments) < Nodes ->
     %Initial N Fragment Creation
-	fragmentize(Parts, Len, Fragments ++ [[lists:nth(random:uniform(length(Parts)), Parts)]], Nodes);
+	fragmentize(Parts, Len, Fragments ++ [[lists:nth(random:uniform(length(Parts)), Parts)]], Nodes, Factor);
 
-fragmentize(Parts, Len, Fragments, Nodes) when Len > 0 ->
+fragmentize(Parts, Len, Fragments, Nodes, Factor) when Len > 0 ->
     %Selecting a value from Parts and calling add3
-	add3(Parts, Len, lists:nth(Len, Parts), Fragments, 3, Nodes);
+    Index = Factor,
+	add3(Parts, Len, lists:nth(Len, Parts), Fragments, Index, Nodes, Factor);
 
-fragmentize(Parts, Len, Fragments, Nodes) when Len == 0 ->
+fragmentize(Parts, Len, Fragments, Nodes, Factor) when Len == 0 ->
 	Fragments.
 
-add3(Parts, Len, Value, Fragments, Index, Nodes) when Index == 0 ->
+add3(Parts, Len, Value, Fragments, Index, Nodes, Factor) when Index == 0 ->
     %Done adding 3 times, calling fragmentize to get the next Value to be distributed
-	fragmentize(Parts, Len-1, Fragments, Nodes-1); %return to fragmentize
+	fragmentize(Parts, Len-1, Fragments, Nodes-1, Factor); %return to fragmentize
 
-add3(Parts, Len, Value, Fragments, Index, Nodes) when Index > 0 ->
+add3(Parts, Len, Value, Fragments, Index, Nodes, Factor) when Index > 0 ->
     %Distributes a Part among 3 random fragments
     Frag = lists:nth(random:uniform(length(Fragments)), Fragments),
     {K, V} = Value,
     Found = lists:keyfind(K, 1, Frag),
     if 
         Found == false -> Temp= lists:delete(Frag, Fragments),
-        add3(Parts, Len, Value, [[Value | Frag]|Temp], Index-1, Nodes);
-        true -> add3(Parts, Len, Value, Fragments, Index, Nodes)
+        add3(Parts, Len, Value, [[Value | Frag]|Temp], Index-1, Nodes, Factor);
+        true -> add3(Parts, Len, Value, Fragments, Index, Nodes, Factor)
     end.
 
