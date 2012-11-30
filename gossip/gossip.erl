47863f2bf9a0a4954d94c595e4eaf935856ec0bf
Sunil <sunil.gandhi007@gmail.com>
2012-11-30 10:26:30 -0500

    Fixed mean of Fragments

--- gossip/gossip.erl
+++ gossip/gossip.erl
@@ -2,19 +2,19 @@
 -import(matrix).
 -import(lister).
 -import(updateFound).
--import(fragreader, [genfrags/1]).
+-import(fragreader, [genfrags/2]).
 -compile(export_all).
 
-start(Function,Input,InputList)->
+start(Function,Input,ReplicationFactor, InputList)->
 	%P = generate_topology()
-	P=matrix:new(10,10,fun (Column, Row, Columns, _) ->                      
+	P=matrix:new(100,100,fun (Column, Row, Columns, _) ->                      
 	Columns * (Row - 1) + Column
 	end),
-	gossip(Function,P,Input,InputList).
+	gossip(Function,P,Input,ReplicationFactor, InputList).
 
-gossip(Function,TransitionMatrix,Input,InputList) ->
-    FragList = genfrags(length(TransitionMatrix)),
-	Pids = create(length(TransitionMatrix),[],TransitionMatrix,Input,Function, FragList,InputList),
+gossip(Function,TransitionMatrix,Input,ReplicationFactor, InputList) ->
+    FragList = genfrags(length(TransitionMatrix),ReplicationFactor),
+	Pids = create(length(TransitionMatrix),[],TransitionMatrix,Input,Function, FragList,ReplicationFactor,InputList),
 	sendpids(length(Pids),Pids),
 	starttimer(Pids,Function).
 	
@@ -32,11 +32,11 @@ sendpids(I,Pids) ->
 	lists:nth(I,Pids) ! {pid, Pids},
 	sendpids(I-1,Pids).
 
-create(0,Pids,TransitionMatrix,Input,Function, FragList,InputList) -> Pids;
-create(I,Pids,TransitionMatrix,Input,Function, FragList,InputList)->
+create(0,Pids,TransitionMatrix,Input,Function, FragList,ReplicationFactor, InputList) -> Pids;
+create(I,Pids,TransitionMatrix,Input,Function, FragList,ReplicationFactor, InputList)->
     Fragment = lists:nth(I, FragList),
-	Pid = spawn_link(fun() -> threadnodes(TransitionMatrix,[],initthread(I,Input,Fragment,Function,InputList), Fragment) end),
-	create(I-1,  (Pids ++ [Pid]), TransitionMatrix, Input,Function, FragList,InputList).
+	Pid = spawn_link(fun() -> threadnodes(TransitionMatrix,[],initthread(I,Input,Fragment,Function,InputList), Fragment,ReplicationFactor) end),
+	create(I-1,  (Pids ++ [Pid]), TransitionMatrix, Input,Function, FragList,ReplicationFactor, InputList).
 
 initthread(I,Input,Fragment,Function,InputList) -> 
 	case Input of 
@@ -55,7 +55,10 @@ case Function of
         max-> [[erlang:max(hd(Myvalue), hd(Value))],Fragment];
         min-> [[erlang:min(hd(Myvalue), hd(Value))],Fragment];
         mean->[[(hd(Myvalue) + hd(Value))/2],Fragment];
-        meanfragments->[[(hd(Myvalue) + hd(Value))/2, (tl(Myvalue) + tl(Value))/(hd(Myvalue) + hd(Value))/2],Fragment];
+        meanfragments->
+        {K1,V1} = hd(Myvalue),
+        {K2,V2} = hd(Value),
+        [[{(K1 +K2)/2, (V1+V2)/2}],Fragment];
         update -> updateFound:upFound(Myvalue, Value,Fragment)
     end.
 
@@ -69,34 +72,34 @@ case Type of
 end.
 	
 
-threadnodes(TransitionMatrix,Pids,Myvalue,Fragment) ->
+threadnodes(TransitionMatrix,Pids,Myvalue,Fragment,ReplicationFactor) ->
     %TODO: Do we need Myvalue as it can be calculated from Fragment?
 	%getneighbours()
 	receive
 		%get the pids of all processes,
         {pid, Pidsmsg } ->
-        	io:format("I am ~p and my value, fragment are ~p | ~p~n ",[self(), Myvalue, Fragment]),
-        	threadnodes(TransitionMatrix,Pidsmsg,Myvalue,Fragment);
+        	%io:format("I am ~p and my value, fragment are ~p | ~p~n ",[self(), Myvalue, Fragment]),
+        	threadnodes(TransitionMatrix,Pidsmsg,Myvalue,Fragment,ReplicationFactor);
         
         %Send Function	This function must be executed after every few minutes
         {send, Function} ->
         	Pid=selectneighbours(TransitionMatrix, Pids, self()),
         	Pid ! {Function, self(), Myvalue},
-        	threadnodes(TransitionMatrix,Pids,Myvalue,Fragment);
+        	threadnodes(TransitionMatrix,Pids,Myvalue,Fragment,ReplicationFactor);
 
         %Recieve Function from Process Pid with his value
         {Function, Pid, Value } ->
         	Pid ! { returnmsg, Function, self(), Myvalue },
         	printmsg(Function, return, [self(), Myvalue, Pid, Value]),
-            threadnodes(TransitionMatrix,Pids, lists:nth(1,calculate( Function, Myvalue,Value,Fragment)),lists:nth(2,calculate( Function, Myvalue,Value,Fragment)));
+            threadnodes(TransitionMatrix,Pids, lists:nth(1,calculate( Function, Myvalue,Value,Fragment)),lists:nth(2,calculate( Function, Myvalue,Value,Fragment)),ReplicationFactor);
 
         %Reply Recieve Function from Process Pid with his value	
         {returnmsg, Function, Pid, Value } ->
             printmsg(Function, returnmsg, [self(),Myvalue,Pid,Value]),
-            threadnodes(TransitionMatrix,Pids, lists:nth(1,calculate( Function, Myvalue,Value,Fragment)),lists:nth(2,calculate( Function, Myvalue,Value,Fragment)));
+            threadnodes(TransitionMatrix,Pids, lists:nth(1,calculate( Function, Myvalue,Value,Fragment)),lists:nth(2,calculate( Function, Myvalue,Value,Fragment)),ReplicationFactor);
             
         {tick, Function}->
        		self() ! {send,Function},
             timer:send_after(1000, {tick, Function}),
-            threadnodes(TransitionMatrix,Pids,Myvalue,Fragment)
+            threadnodes(TransitionMatrix,Pids,Myvalue,Fragment,ReplicationFactor)
     end.
