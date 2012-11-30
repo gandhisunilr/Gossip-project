dfbaaa2a70f73f119e908e7a0e4d0771d0c4be1a
Primal Pappachan <primal007@gmail.com>
2012-11-30 04:17:30 -0500

    Added degree - average and SD

--- gossip/topgen.py
+++ gossip/topgen.py
@@ -6,25 +6,36 @@ import sys
 """
 Generates a topology with N nodes.
 To run: python topgen.py N
-Output: python topgen.py 5
-[[0, 3, 0.25], [0, 0, 0.75]]
-[[1, 2, 0.16666666666666666], [1, 4, 0.16666666666666666], [1, 1, 0.6666666666666667]]
-[[2, 1, 0.16666666666666666], [2, 2, 0.8333333333333334]]
-[[3, 2, 0.25], [3, 3, 0.75]]
-[[4, 1, 0.16666666666666666], [4, 4, 0.8333333333333334]]
+Output: python topgen.py 10
+[[0, 3, 0.1], [0, 6, 0.1], [0, 7, 0.1], [0, 8, 0.1], [0, 0, 0.6]]
+[[1, 4, 0.125], [1, 9, 0.16666666666666666], [1, 1, 0.7083333333333334]]
+[[2, 4, 0.125], [2, 5, 0.16666666666666666], [2, 2, 0.7083333333333334]]
+[[3, 0, 0.1], [3, 6, 0.1], [3, 8, 0.125], [3, 3, 0.675]]
+[[4, 1, 0.125], [4, 2, 0.125], [4, 9, 0.125], [4, 4, 0.625]]
+[[5, 2, 0.16666666666666666], [5, 9, 0.16666666666666666], [5, 5, 0.6666666666666667]]
+[[6, 0, 0.1], [6, 3, 0.1], [6, 7, 0.1], [6, 8, 0.1], [6, 6, 0.6]]
+[[7, 0, 0.1], [7, 2, 0.125], [7, 6, 0.1], [7, 7, 0.675]]
+[[8, 0, 0.1], [8, 3, 0.125], [8, 6, 0.1], [8, 8, 0.675]]
+[[9, 1, 0.16666666666666666], [9, 2, 0.16666666666666666], [9, 9, 0.6666666666666667]]
+
 """
 
-def topology(N):
+""" Degree Variance """
+
+def topology(N, Degree, SD):
 	Cords = generate_coordinates(N)
 	Neighbours = []
 	for i in range(N):
 		Value = Cords[i]
 		MyNeighbours = []
+		Max = Degree + random.randrange(-SD, SD)
 		for j in range(N):
 			V2 = Cords[j]
 			if is_neighbour(calculate_distance(Value[0], Value[1], V2[0], V2[1])):
 				MyNeighbours.append(j)	
-			else: continue
+			elif len(MyNeighbours) > Max: break
+			else: continue 
+
 		if MyNeighbours == []:
 			rand = int(random.random()*10%N) 	
 			index = rand if rand != i else (rand + 1)%N 
@@ -32,10 +43,9 @@ def topology(N):
 		Neighbours.append(MyNeighbours)		
 
 #	print Neighbours	
-	Chain = generate_chain(N, Neighbours)	
-	for C in Chain:
-		print C
+	return generate_chain(N, Neighbours)	
 
+	
 def generate_chain(N, Neighbours):
 
 	Chain = []
@@ -47,12 +57,10 @@ def generate_chain(N, Neighbours):
 			Bone = Neighbours[j]
 			Other = 1/(1 + float((max(len(Nebo),len(Bone)))))
 			SumOfOthers += Other 
-			Element.append([i, j, Other/2]) #Pij/2 Lazy 
-		Element.append([i, i, (1 + (1 - SumOfOthers))/2]) #1 + Pij Lazy 	
+			Element.append([j, Other/2]) #Pij/2 Lazy 
+		Element.append([i, (1 + (1 - SumOfOthers))/2]) #1 + Pij Lazy 	
 		Chain.append(Element)
 	return Chain
-
-
 
 
 def calculate_distance(X0,Y0,X,Y):
@@ -62,11 +70,11 @@ def generate_coordinates(N):
 	return [[random.random(), random.random()] for x in  range(N)]
 
 def is_neighbour(Distance):
-	if Distance >= 0.4 or Distance == 0.0:
+	if Distance >= 0.3 or Distance == 0.0:
 		return False
 	return True
 
 
 if __name__	== "__main__":
 	N = eval(sys.argv[1])
-	topology(N)
+	topology(N, 5, 5)
