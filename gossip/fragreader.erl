7a12eb775b747e3122b5c66a9056b2883e8a17fb
Primal Pappachan <primal007@gmail.com>
2012-12-02 13:12:09 -0500

    Comments added, add3 -> addFactor

--- gossip/fragreader.erl
+++ gossip/fragreader.erl
@@ -1,3 +1,25 @@
+%% 
+% 
+% Reads random numbers stored in a file and generates a list 
+% of fragments with length equal to number of nodes.
+% 
+% Factor parameter determines how many a value would be replicated
+% if given Factor = 3, it means every Value would be replicated 
+% across 3 fragments.
+%
+% Fragments of the form: [{I1, V1},{I2, V2},{I3, V3}........]
+% where I denotes the Index value of the fragment starting from 
+% 1 and till the number of random files in the file and Value denotes
+% the random number read from the file.
+%
+% Working principle: Fragments are created initially by assigning them a 
+% randomly chosen Value. Once N(number of nodes) has been created, each Value 
+% is taken and added to a randomly selected fragment if it doesn't already
+% have that Value.
+%
+% Output = List of Fragments with length(Ouput) = N
+%%
+
 -module(fragreader).
 -compile(export_all).
 
@@ -14,29 +36,29 @@ get_all_lines(Device, Accum, Index) ->
 	end.
 
 fragmentize(Parts, Len, Fragments, Nodes, Factor) when length(Fragments) < Nodes ->
-    %Initial N Fragment Creation
+    %Initial Step: Creating N Fragments
 	fragmentize(Parts, Len, Fragments ++ [[lists:nth(random:uniform(length(Parts)), Parts)]], Nodes, Factor);
 
 fragmentize(Parts, Len, Fragments, Nodes, Factor) when Len > 0 ->
-    %Selecting a value from Parts and calling add3
+    %Selecting a Value from Parts and calling addFactor
     Index = Factor,
-	add3(Parts, Len, lists:nth(Len, Parts), Fragments, Index, Nodes, Factor);
+	addFactor(Parts, Len, lists:nth(Len, Parts), Fragments, Index, Nodes, Factor);
 
 fragmentize(Parts, Len, Fragments, Nodes, Factor) when Len == 0 ->
 	Fragments.
 
-add3(Parts, Len, Value, Fragments, Index, Nodes, Factor) when Index == 0 ->
-    %Done adding 3 times, calling fragmentize to get the next Value to be distributed
+addFactor(Parts, Len, Value, Fragments, Index, Nodes, Factor) when Index == 0 ->
+    %Done adding Factor number of times, calling fragmentize to get the next Value to be distributed
 	fragmentize(Parts, Len-1, Fragments, Nodes-1, Factor); %return to fragmentize
 
-add3(Parts, Len, Value, Fragments, Index, Nodes, Factor) when Index > 0 ->
-    %Distributes a Part among 3 random fragments
+addFactor(Parts, Len, Value, Fragments, Index, Nodes, Factor) when Index > 0 ->
+    %Distributes a Part among 'Factor' number of random fragments
     Frag = lists:nth(random:uniform(length(Fragments)), Fragments),
     {K, V} = Value,
     Found = lists:keyfind(K, 1, Frag),
     if 
         Found == false -> Temp= lists:delete(Frag, Fragments),
-        add3(Parts, Len, Value, [[Value | Frag]|Temp], Index-1, Nodes, Factor);
-        true -> add3(Parts, Len, Value, Fragments, Index, Nodes, Factor)
+        addFactor(Parts, Len, Value, [[Value | Frag]|Temp], Index-1, Nodes, Factor);
+        true -> addFactor(Parts, Len, Value, Fragments, Index, Nodes, Factor)
     end.
 
