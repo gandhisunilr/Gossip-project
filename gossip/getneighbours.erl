623544b2cecf4f42f38a906aceccbc70e8ffdb41
Sunil <sunil.gandhi007@gmail.com>
2012-12-02 15:49:03 -0500

    From branch master

---
+++ gossip/getneighbours.erl
@@ -0,0 +1,15 @@
+-module(getneighbours).
+-compile(export_all).
+-import(matrix).
+
+getneighbours(FileName) ->
+    {ok, Device} = file:open(FileName, [read]),
+    get_all_lines(Device, []).
+
+get_all_lines(Device, Accum) ->
+	Tuple = io:fread(Device, "","~d,~f "),
+    case Tuple of
+    	eof -> file:close(Device), Accum;
+    	{ok, [I, P]} -> 
+            get_all_lines(Device, Accum ++ [{I,P}])
+    end.
