77db3d940319ff67dd29aace22784d05abedbca3
Sunil <sunil.gandhi007@gmail.com>
2012-12-02 17:02:46 -0500

    Fixed regex

--- gossip/findconvergence.py
+++ gossip/findconvergence.py
@@ -8,13 +8,13 @@ def findconvergence(Networksize,ActualValue, Percenterror):
 	for line in iter(f):
 		I = I +1
 		Myvalue = float(line.split(" ")[1])
-		Pid = int(re.findall(r'<0.(.*).0>',line.split(" ")[0])[0])
-		if (Myvalue - ActualValue) < threshold:
+		Pid = int(re.findall(r'<0.(.*).[0-9]>',line.split(" ")[0])[0])
+		if abs(Myvalue - ActualValue) < threshold:
 			Pidmapping[Pid]= 1
 		else:
 			Pidmapping[Pid]= 0
 
-		if (len(Pidmapping) > Networksize) and (sum(Pidmapping.values()) > Networksize):
+		if (len(Pidmapping) >= Networksize) and (sum(Pidmapping.values()) >= Networksize):
 			print "Converged at Line"+str(I)
 			break
 	print "Done Checking"
